f586f6660f80dd15a5c249ababb3c30680ce308a
Steven Hall <Hallzy.18@gmail.com>
2015-07-29 14:48:30 -0700

    Made changes to gameplay.c

--- src/gameplay.c
+++ src/gameplay.c
@@ -6,45 +6,8 @@
 #include "scoring.h"
 #include "initialization.h"
 
-void playPlayersTurn() {
-  player_t* player = getPlayer();
-  char choice;
-  // Show the players card
-  show_hand(player);
-  do {
-    printf("\n(h)it or (s)tay? ");
-    // Note the space before %c, which consumes the newline character after
-    // hitting enter
-    scanf(" %c", &choice);
-    printf("\n");
-
-    // If player hits, give them a new card and show the players current hand.
-    if (choice == 'h' || choice == 'H') {
-      deal_card(player);
-      show_hand(player);
-
-      // After a hit, if the player has more than 21 then exit because they
-      // lost.
-      if (getScore(player) > 21) {
-        return;
-      }
-    }
-    // if they hit, loop and do it again to see of they want to hit or stay
-    // again.
-  } while (choice == 'h' || choice == 'H');
-}
-
 static bool dealer_needs_more_points() {
   return getScore(getDealer()) < 17;
-}
-
-void playDealersTurn() {
-  // Hit for the dealer and get score and compare with player - dealer hits when
-  // hand is < 17
-  while (dealer_needs_more_points()) {
-    deal_card(getDealer());
-  }
-
 }
 
 bool player_busts() {
@@ -66,6 +29,45 @@ static bool dealerWins() {
 static bool playerWins() {
   return getScore(getDealer()) < getScore(getPlayer());
 }
+
+void playPlayersTurn() {
+  player_t* player = getPlayer();
+  char choice;
+  // Show the players card
+  show_hand(player);
+  do {
+    printf("\n(h)it or (s)tay? ");
+    // Note the space before %c, which consumes the newline character after
+    // hitting enter
+    scanf(" %c", &choice);
+    printf("\n");
+
+    // If player hits, give them a new card and show the players current hand.
+    if (choice == 'h' || choice == 'H') {
+      deal_card(player);
+      show_hand(player);
+
+      // After a hit, if the player has more than 21 then exit because they
+      // lost.
+      if (player_busts()) {
+        return;
+      }
+    }
+    // if they hit, loop and do it again to see of they want to hit or stay
+    // again.
+  } while (choice == 'h' || choice == 'H');
+}
+
+
+void playDealersTurn() {
+  // Hit for the dealer and get score and compare with player - dealer hits when
+  // hand is < 17
+  while (dealer_needs_more_points()) {
+    deal_card(getDealer());
+  }
+
+}
+
 
 void determineWinner() {
   player_t* player = getPlayer();
